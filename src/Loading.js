9627fa8d8b532e6b4176700dc660d92a1089778a
Ashley Mathew <amathew195@gmail.com>
2023-01-10 11:06:07 -0600

    added loading component and css for component

---
+++ src/Loading.js
@@ -0,0 +1,15 @@
+import './Loading.css'
+
+/**
+ * This component renders a loading page.
+ */
+
+function Loading() {
+  return (
+      <div class="Loading" >
+        <p>Loading...</p>
+      </div>
+  );
+}
+
+export default Loading;
